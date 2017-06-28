f0b9368de5a9a6a36d3e7af2ee496dfe484923d2
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2017-06-28 19:42:31 -0300

    Projeto estável final 1.0

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# CRUD---JPA-JSP-JAVA
+# br.com.lucas
+Projeto universitário de CRUD com JPA
+
+## Obtendo o projeto
+
+`git clone https://github.com/lucasviniciosfs/CRUD---JPA-JSP-JAVA`
+
+## Executando a aplicação
+
+`mvn org.apache.tomcat.maven:tomcat7-maven-plugin:run -Dmaven.tomcat.port=9090`
+
+ou
+
+`./mvnw tomcat7:run`
+
+## Acessando a aplicação
+
+Acesse `http://localhost:9090/br.com.lucas/br.com.lucas.crudAluno/aluno` em qualquer navegador.
+
+## Como o projeto foi construído
+
+### "Embutindo" o Maven
+
+Para que não seja necessário instalar e configurar o Maven, ele foi embutido no projeto com o seguinte comando:
+
+`mvn io.takari:maven:0.3.3:wrapper -Dmaven=3.3.9`
---
+++ src/main/java/lab/academico/AlunoController.java
@@ -0,0 +1,60 @@
+package lab.academico;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/br.com.lucas.crudAluno/aluno")
+public class AlunoController extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String valor(HttpServletRequest req, String param, String padrao) {
+		String result = req.getParameter(param);
+		if (result == null) {
+			result = padrao;
+		}
+		return result;
+	}
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			String msg;
+			String op = valor(req, "operacao", "");
+			String matricula = valor(req, "matricula", "");
+			String nome = valor(req, "nome", "");
+			if (op.equals("incluir")) {
+				AlunoDao.inclui(matricula, nome);
+				msg = "Inclusão realizada com sucesso.";
+			} else if (op.equals("alterar")) {
+				try {
+					int matri = Integer.parseInt(matricula);
+				} catch (Exception e) {
+					throw new IllegalArgumentException("Matrícula não pode ser alterada!");
+				}
+				AlunoDao.alterar(matricula, nome);
+				msg = "Alteração realizada com sucesso.";
+			} else if (op.equals("excluir")) {
+				AlunoDao.excluir(matricula);
+				msg = "Exclusão realizada com sucesso.";
+			} else if (op.equals("")) {
+				msg = "";
+			} else {
+				throw new IllegalArgumentException("Operação \"" + op + "\" não suportada.");
+			}
+			req.setAttribute("msg", msg);
+			req.setAttribute("alunos", AlunoDao.listar());
+			
+			req.getRequestDispatcher("AlunoView.jsp").forward(req, resp);
+		} catch (Exception e) {
+			e.printStackTrace(resp.getWriter());
+		}
+	}
+}
